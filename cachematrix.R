a11bd32cc8507df42281275c0d02d1ffb68db410
PenchalaP <58372482+PenchalaP@users.noreply.github.com>
2019-12-02 23:33:29 +0530

    Corrected typo error

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,18 @@
 makeCacheMatrix <- function(x = matrix()) {
       
   invers <- NULL
+  # set the value of the matrix
   set <- function(y){
     x <<- y
     invers <<- NULL
   }
+   # get the value of the matrix    
   get <- function() x
+   # set the value of the inverse   
   setInverse <- function(solveMatrix) invers <<- solveMatrix
+   # get the value of the inverse   
   getInverse <- function() invers
+      
   list(set = set, get =get, setInverse =setInverse, getInverse = getInverse)
 
 }
@@ -21,13 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        
+    ## Return a matrix that is the inverse of 'x'
+    # check to see if the inverse is already there    
+   
    invers <- x$getInverse()
+    # check if the inverse is there, then return it  
   if(!is.null(invers)){
     message("************* Cached Data **********************")
     return (invers)
   }
+  # otherwise, solve the matrix and produce the inverse    
   data <- x$get()
   invers <- solve(data)
   x$setInverse(invers)
